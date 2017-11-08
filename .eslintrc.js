afe845dde66676ab185de34617af650e62ac6133
jarett-lee <jarett.lee.pi@gmail.com>
2017-11-08 14:02:30 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  "env": {
+    "node": true,
+    "es6": true,
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2,
+    ],
+    "linebreak-style": [
+      "error",
+      "unix",
+    ],
+    "quotes": [
+      "error",
+      "single",
+    ],
+    "semi": [
+      "error",
+      "always",
+    ],
+    "comma-dangle": [
+      "error",
+      "always-multiline",
+    ],
+    "no-console": "off",
+  },
 };
