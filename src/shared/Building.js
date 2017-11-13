cdbb4ba10adb9304f98073efa7bd9cb3ffefe47d
JordanR159 <reed226@purdue.edu>
2017-11-12 22:55:06 -0500

    Add tile class, creep types, and building types

--- src/shared/Building.js
+++ src/shared/Building.js
@@ -2,50 +2,97 @@
 
 class Building
 {
-    var xpos;
-    var ypos;
-    var type;
+    /*
+    Integer xposition
+    Integer yposition
+    Integer towerLevel
+    String buildingType
+    */
 
     constructor(xpos, ypos, type)
     {
-        this.xpos = xpos;
-        this.ypos = ypos;
-        this.type = type;
+        this.xposition = xpos;
+        this.yposition = ypos;
+        this.buildingType = type;
     }
 
-    get xposition()
+    upgrade()
     {
-        return this.xpos;
-    }
 
-    get yposition()
-    {
-        return this.ypos;
-    }
-
-    get type()
-    {
-        return this.type;
     }
 }
 
 class OffenseTower extends Building
 {
-    var creepType;
+    /*
+    Creep creepType
+    Float bossMultiplier <-- = 1 if normal creep, > 1 if boss creep 
+    */
+
+    constructor(xpos, ypos, type)
+    {
+        super(xpos, ypos, type);
+        switch (type)
+        {
+            case "CreeperSpawn":
+                break;
+            case "QuicksterSpawn":
+                break;
+            case "SwarmieSpawn":
+                break;
+            case "RotundoSpawn":
+                break;
+            case "MassimoSpawn":
+                break;
+        }
+    }
 
     // Spawns the creep associated with the tower type
     spawn()
     {
 
     }
+
+    upgrade()
+    {
+        super.upgrade();
+    }
 }
 
 class DefenseTower extends Building
 {
-    var baseDamage;
-    var baseRate;
-    var baseRange;
-    var priority; // Closest to tower, lowest health, etc.
+    /*
+    Integer baseDamage
+    Float multDamage
+    Integer baseRate
+    Float multRate
+    Integer baseRange
+    Float multRange
+    String priority
+    */
+
+    constructor(xpos, ypos, type)
+    {
+        super(xpos, ypos, type);
+        switch (type)
+        {
+            // Basic tower
+            case "BasicTower":
+                break;
+            // Fast shooting tower, good on Quicksters
+            case "AirTower":
+                break;
+            // Splash damage tower, good on Swarmies
+            case "WaterTower":
+                break;
+            // High single shot damage tower, good on Rotundos and Massimos
+            case "EarthTower":
+                break;
+            // Shoots fast, splash, and high damage. Good on everything
+            case "FireTower":
+                break;
+        }
+    }
 
     // Calculate straight line distance from tower to a creep
     distanceFrom(creep)
@@ -58,18 +105,45 @@ class DefenseTower extends Building
     {
 
     }
+
+    upgrade()
+    {
+        super.upgrade();
+    }
 }
 
 class PowerNode extends Building
 {
-    var baseIncome;
-    var currHealth;
-    var maxHealth;
+    /*
+    Integer baseIncome
+    Integer currHealth
+    Integer maxHealth
+    */
+
+    constructor(xpos, ypos, type, health)
+    {
+        super(xpos, ypos, type);
+        this.currHealth = health;
+        this.maxHealth = health;
+        calculateIncome();
+    }
+
+    // Calculates the base income of the node using factors such as 
+    // number of players, type of node, etc.
+    calculateIncome()
+    {
+
+    }
 
     // Generates income
     generate()
     {
 
+    }
+
+    upgrade()
+    {
+        super.upgrade
     }
 }
 
--- src/shared/Creep.js
+++ src/shared/Creep.js
@@ -2,66 +2,45 @@
 
 class Creep
 {
-    var xposition;
-    var yposition;
-    var creepType;
-    var currHealth;
-    var maxHealth;
-    var baseSpeed;
-    var baseDamage;
-    var baseRange;
+    /*
+    Integer xposition
+    Integer yposition
+    String creepType
+    Integer creepSize
+    Integer creepLevel
+    Integer currHealth
+    Integer maxHealth
+    Integer baseSpeed
+    Float multSpeed
+    Integer baseDamage
+    Float multSpeed
+    Integer baseRange
+    Float multSpeed
+    */
 
     // Build creep
-    constructor(xpos, ypos, type)
+    constructor(xpos, ypos, type, level, bossMultiplier)
     {
         this.xposition = xpos;
         this.yposition = ypos;
         switch(type)
         {
-            case "Regular":
+            // Basic creep
+            case "Creeper":
                 break;
-            case "Speedster":
+            // Creep with higher base speed, but less health
+            case "Quickster":
                 break;
-            case "Swarmster":
+            // Smaller creep that spawns in hords, but less health and damage
+            case "Swarmie":
                 break;
-            case "Toughster":
+            // Creep with highest health, but less base speed
+            case "Rotundo":
+                break;
+            // Larger creep with higher health and damage, but lowest base speed
+            case "Massimo":
                 break;
         }
-    }
-
-    get xposition()
-    {
-        return this.xposition;
-    }
-
-    get yposition()
-    {
-        return this.yposition;
-    }
-
-    get creepType()
-    {
-        return this.creepType;
-    }
-
-    get currHealth()
-    {
-        return this.currHealth;
-    }
-
-    get maxHealth()
-    {
-        return this.maxHealth;
-    }
-
-    get speed()
-    {
-        return this.baseSpeed;
-    }
-
-    get range()
-    {
-        return this.baseRange;
     }
 
     // Function to deal damage to a power node in range
