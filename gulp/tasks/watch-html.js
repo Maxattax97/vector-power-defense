9231d577f92f0e1d318429522d996c624c7e8467
Max O'Cull <maxattax97@gmail.com>
2017-12-01 00:12:31 -0500

    Initialize experimental authentication and database over HTTPS

--- gulp/tasks/watch-html.js
+++ gulp/tasks/watch-html.js
@@ -1,8 +1,14 @@
 const gulp = require('gulp');
 const watch = require('gulp-watch');
 
+const glob = [
+    'src/server/**/*.html',
+    'src/client/**/*.html',
+    'src/shared/**/*.html',
+];
+
 module.exports = function() {
-    return watch('src/client/index.html', function() {
+    return watch(glob, function() {
         gulp.start('build-html');
     });
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,26 @@
-const express = require('express');
-const app = express();
+var bodyParser = require("body-parser");
+var express = require("express");
+var app = express();
 
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
+var publicHtmlDir = __dirname + "/../client/"
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+// Probably a bad idea... HTTPS.
+app.get("/", function(req, res) {
+    // Check for cookie, if no cookie respond with login.
+    console.log(req.headers, req.header);
+    res.sendFile(publicHtmlDir + "login.html");
+});
+
+app.post("/auth", function(req, res) {
+    console.log(req.body);
+    res.send("Received a POST request at /auth");
+});
 
 app.listen(port);
